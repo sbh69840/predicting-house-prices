04fb8f1c3bf2236ee2613350385f9dd1ae3c77dc
dsu <sbh69840@gmail.com>
2018-09-04 12:50:27 +0530

    train-train-test-split

--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 import pandas 
 import numpy as np 
+from sklearn.model_selection import train_test_split
 train = pandas.read_csv('train.csv')
 test = pandas.read_csv('test.csv')
 #creating a model using only bath,balcony as features and
@@ -19,15 +20,20 @@ train['balcony'] = train['balcony'].apply(lambda x: x/3)
 test['bath'] = test['bath'].apply(lambda x: x/40)
 test['balcony'] = test['balcony'].apply(lambda x: x/3)
 
-print(train.head())
-print(test.head())
 train['price'] = np.log1p(train['price'])
-print(train.head())
 
 
+X = train[['bath','balcony']].values
+y = train['price'].values
+print(X.shape)
 #Cleaning done
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.1,stratify=y,random_state=7)
 
 #now let's create a simple model that accepts integer and return
+from keras.models import Sequential 
+from keras.layers import Conv1D,MaxPooling1D,Dropout,Dense,Flatten
 
+model = Sequential()
+#model.add(Conv1D(32,3,input_shape=(2,1),activation='relu'))
 
 
