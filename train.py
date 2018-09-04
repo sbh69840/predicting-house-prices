8eaf4da67ace75d5558f57d7bc9646c0f7ee4707
dsu <sbh69840@gmail.com>
2018-09-04 12:19:49 +0530

    train-scaling-price-of-house

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 import pandas 
+import numpy as np 
 train = pandas.read_csv('train.csv')
 test = pandas.read_csv('test.csv')
 #creating a model using only bath,balcony as features and
@@ -8,18 +9,10 @@ train = train.drop(['area_type','availability','location','size','society','tota
 test = test.drop(['area_type','availability','location','size','society','total_sqft'],axis=1)
 
 #Handling nan values, for now it is mean of the column
-print(train['bath'].shape)
 train['bath'] = train['bath'].fillna(train['bath'].mean())
 train['balcony'] = train['balcony'].fillna(train['balcony'].mean())
 test['bath'] = test['bath'].fillna(test['bath'].mean())
 test['balcony'] = test['balcony'].fillna(test['balcony'].mean())
-print(train.isnull().sum())
-print(test.isnull().sum())
-print("maximum of bath in train is: = {0}".format(train['bath'].max()))
-print("maximum of balcony in train is: = {0}".format(train['balcony'].max()))
-
-print("maximum of bath in test is: = {0}".format(train['bath'].max()))
-print("maximum of balcony in test is: = {0}".format(train['balcony'].max()))
 
 train['bath'] = train['bath'].apply(lambda x: x/40)
 train['balcony'] = train['balcony'].apply(lambda x: x/3)
@@ -28,6 +21,9 @@ test['balcony'] = test['balcony'].apply(lambda x: x/3)
 
 print(train.head())
 print(test.head())
+train['price'] = np.log1p(train['price'])
+print(train.head())
+
 
 #Cleaning done
 
