3816aaeff2b29dbd7c5607d44a2b90ab92bce4a5
dsu <sbh69840@gmail.com>
2018-09-04 12:03:13 +0530

    train-cleaning-the-data-1.0

--- train.py
+++ train.py
@@ -7,3 +7,22 @@ print(train.dtypes)
 print(train.isnull().sum())
 print(test.dtypes)
 print(test.isnull().sum())
+#creating a model using only bath,balcony as features and
+#price as target
+#Removing unnecessary columns for the current version 
+train = train.drop(['area_type','availability','location','size','society','total_sqft'],axis=1)
+test = test.drop(['area_type','availability','location','size','society','total_sqft'],axis=1)
+print(train.dtypes)
+print(train.isnull().sum())
+print(test.isnull().sum())
+
+#Handling nan values, for now it is mean of the column
+print(train['bath'].shape)
+train['bath'] = train['bath'].fillna(train['bath'].mean())
+train['balcony'] = train['balcony'].fillna(train['balcony'].mean())
+test['bath'] = test['bath'].fillna(test['bath'].mean())
+test['balcony'] = test['balcony'].fillna(test['balcony'].mean())
+print(train.isnull().sum())
+print(test.isnull().sum())
+
+
