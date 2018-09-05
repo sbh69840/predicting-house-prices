b2ce932add91ffc064649f4f7d5786c9c3b40db5
dsu <sbh69840@gmail.com>
2018-09-05 17:38:37 +0530

    train-sklearn-regressor-default-parameters-gridsearch-final

--- train.py
+++ train.py
@@ -21,47 +21,55 @@ test['bath'] = test['bath'].apply(lambda x: x/40)
 test['balcony'] = test['balcony'].apply(lambda x: x/3)
 
 train['price'] = np.log1p(train['price'])
-X_testing = test[['bath','balcony']].values.reshape(-1,2,1)
+X_testing = test[['bath','balcony']].values
 
 X = train[['bath','balcony']].values
 y = train['price'].values
-print(X.shape)
 #Cleaning done
-print(y.shape)
 X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.1,random_state=7)
-print(X_train.shape)
-#now let's create a simple model that accepts integer and return
-from keras.models import Sequential 
-from keras.layers import Conv1D,MaxPooling1D,Dropout,Dense,Flatten
-from keras.callbacks import EarlyStopping,ModelCheckpoint 
-#Reshape train and validation data to 3 dimenstional, since 
-#The input shape is =(batch_size,2,1)
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import mean_squared_error 
+#Method to accept a regressor, fit it and predict, and give the best value possible.
+def regression(X_train,y_train,X_test,y_test,regressor,parameters):
+    reg = GridSearchCV(regressor,parameters,n_jobs=-1,cv=10,scoring="neg_mean_squared_error")
+    reg.fit(X_train,y_train)
+    mse = mean_squared_error(y_test,reg.predict(X_test))
+    print(mse)
+    return reg.best_estimator_,mse
 
-X_train = np.expand_dims(X_train,axis=2)
-print(X_train.shape)
-X_test = np.expand_dims(X_test,axis=2)
-print(X_test.shape)
-
-#Simple model wih 2 Layer convolution1D
-model = Sequential()
-model.add(Conv1D(32,2,input_shape=(2,1),activation='relu'))
-model.add(Conv1D(32,1,activation='relu'))
-model.add(Flatten())
-model.add(Dense(1))
-
-# save = ModelCheckpoint('model.h5',monitor='val_loss',save_best_only=True,mode='min')
-# early = EarlyStopping(monitor='val_loss',mode='min',patience=5)
-# model.compile(optimizer='adam',loss='mse')
-
-# model.fit(X_train,y_train,validation_data=(X_test,y_test),verbose=1,batch_size=64,epochs=100,callbacks=[early,save])
+from sklearn.ensemble import GradientBoostingRegressor,BaggingRegressor
+from sklearn.neighbors import KNeighborsRegressor 
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import ExtraTreesRegressor
+from sklearn.tree import DecisionTreeRegressor 
+from sklearn.ensemble import AdaBoostRegressor 
 
 
-#The underlying code is for creaating the sbmission,
+ens = [GradientBoostingRegressor(),BaggingRegressor(),KNeighborsRegressor(),\
+       RandomForestRegressor(),ExtraTreesRegressor(),DecisionTreeRegressor(),\
+       AdaBoostRegressor()]
+scores = {}
+for a in ens:
+    reg,mse = regression(X_train,y_train,X_test,y_test,a,{})
+    scores[a] = mse 
+maxi = 1
+for a in scores:
+    if (scores[a]<maxi):
+        print(scores[a],a)
+        maxi = scores[a]
+#According to the above code, GradientBoostingRegressor is the best with default parameters,
+reg,mse = regression(X_train,y_train,X_test,y_test,GradientBoostingRegressor(),{})
+
+    
+    
+
+
+#The underlying code is for creating the sbmission,
 #since the hackathon holders expect me to submit in xl form I am using
 #ExcelWriter of pandas 
-from keras.models import load_model 
-model = load_model('model.h5')
-pred = model.predict(X_testing)
+# from keras.models import load_model 
+# model = load_model('model.h5')
+pred = reg.predict(X_testing)
 pred = pred.reshape(-1,)
 prediction = []
 for a in pred:
@@ -70,7 +78,9 @@ print(prediction)
 sub = pandas.DataFrame()
 sub['price'] = prediction
 print(sub.head()) 
-writer = pandas.ExcelWriter('submit.xlsx')
+writer = pandas.ExcelWriter('submit1-sklearn.xlsx')
 sub.to_excel(writer,index=False)
 writer.save()
-#The score with this model out of 1 was 0.78247318 and I was placed at 63rd position.
+#The score with this sklearnregressor,default parameters out of 1 was 0.78336541, which is
+# 0.01 score increase from previos COnv1D single layer nueral network and I was placed at 63rd position
+#even with this score.
